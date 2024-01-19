e42b31e993728d8ea12a972e7fe1f1715c428505
manmulla <manoj.mulla@gmail.com>
2024-01-19 23:55:27 +0530

    Deployment configuration for AWS

--- src/utils.py
+++ src/utils.py
@@ -22,7 +22,6 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -33,7 +32,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
-
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
@@ -55,15 +53,11 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
-    def load_object(file_path):
-        try:
-            with open(file_path, "rb") as file_obj:
-                return pickle.load(file_obj)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
-        except Exception as e:
-            raise CustomException(e, sys)
-        
-        
-    
-    
+    except Exception as e:
+        raise CustomException(e, sys)
     
